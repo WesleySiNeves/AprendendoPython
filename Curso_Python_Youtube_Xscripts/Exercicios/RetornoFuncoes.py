78c5aecb11d863147648659a3729bdfc859090b7
Wesley Neves <wesley.neves@implantainformatica.com.br>
2020-01-06 13:19:12 -0300

    comit

---
+++ Curso_Python_Youtube_Xscripts/Exercicios/RetornoFuncoes.py
@@ -0,0 +1,14 @@
+
+def  Calcula(numero):
+    return numero,(numero + numero), (numero ** numero),(numero / numero)
+
+
+
+digiteUmNumero  = int(input("digite um numero:"))
+
+print((Calcula(digiteUmNumero)))
+
+
+
+
+
