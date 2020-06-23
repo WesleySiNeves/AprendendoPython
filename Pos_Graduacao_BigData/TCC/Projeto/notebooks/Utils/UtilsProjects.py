95599e6da3771f5107797b22760c125190d0706e
Wesley Everton Jesus Neves <Wesley.Neves@implantainformatica.com.br>
2020-06-23 08:52:58 -0300

    comit

---
+++ Pos_Graduacao_BigData/TCC/Projeto/notebooks/Utils/UtilsProjects.py
@@ -0,0 +1,75 @@
+
+
+
+
+
+class NetWorkConfiguration(object):
+    def __init__(self):
+        self.IdModelo =""
+        self.NomeModelo=""
+        self.TamanhoModelo=""
+        self.EPOCHS=""
+        self.INIT_LR=""
+        self.BS=""
+        self.IMAGE_SIZE=""
+        self.Descricao=""
+        self.DataCriacao=""
+        self.QuantidadeImagensTotais=""
+        self.QuantidadeImagensCovid=""
+        self.QuantidadeImagensPneumonia=""
+        self.QuantidadeImagensNormal =""
+        self.QuantidadeImagensTrainX=""
+        self.QuantidadeImagensTestX=""
+        self.QuantidadeImagensTrainY=""
+        self.QuantidadeImagensTestY=""
+        self.Rotation_range=""
+        self.Fill_mode=""
+        self.Test_size=""
+        self.TipoCNN=""
+        self.NomeNoteBook = ""
+        self.Acuracia = ""
+        self.Sensitividade =""
+        self.Especificidade =""
+        self.DiretorioProjeto=""
+
+    def to_dictionary(self):
+        self.NomeModelo = self.get_ModelName()
+        return {
+            'IdModelo' : self.IdModelo,
+            'NomeModelo' :self.NomeModelo,
+            'TamanhoModelo':self.TamanhoModelo,
+            'EPOCHS' :self.EPOCHS,
+            'INIT_LR' :self.INIT_LR,
+            'BS' : self.BS,
+            'IMAGE_SIZE' :self.IMAGE_SIZE,
+            'Descricao' :self.Descricao,
+            'DataCriacao' :self.DataCriacao,
+            'QuantidadeImagensTotais' :self.QuantidadeImagensTotais,
+            'QuantidadeImagensCovid' :self.QuantidadeImagensCovid,
+            'QuantidadeImagensPneumonia' :self.QuantidadeImagensPneumonia,
+            'QuantidadeImagensNormal' :self.QuantidadeImagensNormal,
+            'QuantidadeImagensTrainX' :self.QuantidadeImagensTrainX,
+            'QuantidadeImagensTestX' :self.QuantidadeImagensTestX,
+            'QuantidadeImagensTrainY' :self.QuantidadeImagensTrainY,
+            'QuantidadeImagensTestY' :self.QuantidadeImagensTestY,
+            'Rotation_range' :self.Rotation_range,
+            'Fill_mode' :self.Fill_mode,
+            'Test_size' :self.Test_size,
+            "TipoCNN" :self.TipoCNN,
+            "NomeNoteBook" :self.NomeNoteBook,
+            "Acuracia" :self.Acuracia,
+            "Sensitividade" :self.Sensitividade,
+            "Especificidade" :self.Especificidade,
+
+        }
+    def get_ModelName(self):
+      self.NomeModelo = "Model_{0}_{1}_{2}_EPOCHS_{3}_BS_{4}_ACC_{5}_Sens_{6}_Espec".format(config.TipoCNN,config.NomeNoteBook,config.EPOCHS,config.BS,config.Acuracia,config.Sensitividade,config.Especificidade) 
+      return self.NomeModelo
+
+    def get_propriedades(self):
+      return [i for i in self.__dict__.keys() if i[:1] != '_']
+
+    # Métodos são funções, que recebem como parâmetro atributos do objeto criado    
+    def getInformacoes(self):
+        print('Modelo {0}|{1} criado em {2} |com {3} EPOCHS|{4} IMG_COVID| {5} IMGPNEU|{6} IMGNORMAL'.
+        format(self.IdModelo,self.NomeModelo,str(self.DataCriacao),self.EPOCHS,self.QuantidadeImagensCovid,self.QuantidadeImagensPneumonia,self.QuantidadeImagensNormal))
